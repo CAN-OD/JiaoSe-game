765bfc402e47ff0e8126d3438fffa8986c3fe4d6
魏万斌 <824030278@163.com>
2016-09-29 10:10:51 +0800

    no message

--- js/JiaoSeClass.js
+++ js/JiaoSeClass.js
@@ -4,13 +4,16 @@
 
 // 角色基类
 var JiaoSe = function (){
-    this.x = 0;
-    this.y = 0;
+    this.x = 0; // 初始横坐标
+    this.y = 0; //  初始纵坐标
     this.size = 32;// 角色的大小
-    this.dir = "";// 方向0：上 1：下 2：左3：右
-    this.speed = 1; //角色克的速度
+    this.dir = "";// 方向0、w：上, 1、s：下, 2、a：左,3、d：右
+    this.speed = 1; //角色的移动速度速度
     this.cssFile = ""; // css样式文件
     this.JingTai = ""; // 静态图片
+    this.blood = 100;  // 人物血量
+    this.blue = 100;  // 技能蓝量
+
 
     this.tempX = 0;
     this.tempY = 0;
